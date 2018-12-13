0e038d84bda67e4f3f00dae6f3385a8ce9477a07
Tristan Conant <gritcity@tristans-mbp.local>
2018-12-13 12:27:28 -0800

    Hidding 'Tags' on sinlge priduct.

--- wp-content/themes/bbcharcoal/woocommerce/single-product/meta.php
+++ wp-content/themes/bbcharcoal/woocommerce/single-product/meta.php
@@ -34,7 +34,7 @@ global $product;
 
 	<?php echo wc_get_product_category_list( $product->get_id(), ', ', '<span class="styled-cat"><span class="posted_in">' . _n( '', '', count( $product->get_category_ids() ), 'woocommerce' ) . ' ', '</span></span>' ); ?>
 
-	<?php echo wc_get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
+	<?php echo wc_get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">' . _n( '', '', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
 
