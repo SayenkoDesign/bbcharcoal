734a839bfdc229d9a510cbef73a3de18050c4c09
Tristan Conant <gritcity@tristans-mbp.local>
2019-01-04 09:57:56 -0800

    External post title was linking to post rather than external URL

--- wp-content/themes/bbcharcoal/template-parts/content-external-post-column.php
+++ wp-content/themes/bbcharcoal/template-parts/content-external-post-column.php
@@ -9,34 +9,35 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-	<?php        
+
+	<?php
     $post_image = get_the_post_thumbnail_url( get_the_ID(), 'large' );
+		$external_url = get_field( 'link' );
     if( !empty( $post_image ) ) {
         $post_image = sprintf( 'background-image: url(%s);', $post_image );
-    }        
-    $post_title = sprintf( '<h3><a href="%s">%s</a></h3>', get_permalink(), get_the_title() );
+    }
+    $post_title = sprintf( '<h3><a href="%s">%s</a></h3>', $external_url, get_the_title() );
 
     printf( '<div class="post-hero" style="%s"></div>', $post_image );
-                
-        
+
+
 	?>
-	
+
 	<div class="entry-content" data-equalizer-watch>
-	
-		<?php 
-	
+
+		<?php
+
         printf( '<header class="entry-header">%s</header>', $post_title );
-        
+
         echo apply_filters( 'the_content', get_the_content() );
 		?>
-		
+
 	</div><!-- .entry-content -->
-    
+
     <footer class="entry-footer">
-        <?php 
+        <?php
         printf( '<p class="read-more"><a href="%s" class="more">%s</a></p>', get_field( 'link' ), __( 'Read More', '_s' ) ) ;
-        ?>         
+        ?>
 	</footer><!-- .entry-footer -->
-    
+
 </article><!-- #post-## -->
